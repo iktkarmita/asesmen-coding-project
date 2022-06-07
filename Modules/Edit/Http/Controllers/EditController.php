951e2e489a3e0579bed3407c03793e5101d123ab
i ketut karmita <100656272+iktkarmita@users.noreply.github.com>
2022-06-07 16:55:17 +0800

    menambahkan fitur edit atau update pada admin

---
+++ Modules/Edit/Http/Controllers/EditController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Modules\Edit\Http\Controllers;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Modules\Edit\Repositories\EditRepository;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+use Modules\Edit\Models\Produks;
+
+class EditController extends Controller
+{
+    /**
+    
+     * @return void
+     */
+    private $EditRepository;
+    public function __construct(EditRepository $EditRepository)
+    {
+        $this->middleware('auth');
+        $this->EditRepository = $EditRepository;
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable  
+     */
+
+    public function index()
+    {
+
+
+        $role = Auth::user()->role;
+
+        if ($role == '1') {
+            $Produk = produks::all();
+            return view('edit::indexx', compact(['Produk']));
+        } else {
+            return redirect('/home');
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     * @return Renderable
+     */
+    public function create()
+    {
+        return view('edit::create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @param Request $request
+     * @return Renderable
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Show the specified resource.
+     * @param int $id
+     * @return Renderable
+     */
+    public function show($id)
+    {
+        return view('edit::show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * @param int $id
+     * @return Renderable
+     */
+    public function edit($id)
+    {
+        $role = Auth::user()->role;
+
+        if ($role == '1') {
+            $Produk = $this->EditRepository->findById($id);
+
+            return view('edit::editt', compact(['Produk']));
+        } else {
+            return redirect('/home');
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * @param Request $request
+     * @param int $id
+     * @return Renderable
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param int $id
+     * @return Renderable
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
