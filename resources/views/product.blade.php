1ec08021986eb8cdbf05d88ca0080071b3c6442d
i ketut karmita <100656272+iktkarmita@users.noreply.github.com>
2022-06-08 09:18:42 +0800

    membuat class untuk vue,js

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -2,84 +2,11 @@
  @section('styles')
  @endsection
  @section('content')
-     <!--====== SLIDER PART START ======-->
-    
-     <section id="home" class="slider-area pt-100">
-        <div class="container-fluid position-relative">
-            <div class="slider-active">
-                <div class="single-slider">
-                    <div class="">
-                        <div class="row no-gutters align-items-center ">
-                            <div class="col-lg-4 col-md-5">
-                                <div class="slider-product-image d-none d-md-block">
-                                    <img src="assets/images/slider/kemeja1.jpg" alt="Slider">
-                                    <div class="slider-discount-tag">
-                                        <p>-20% <br> OFF</p>
-                                    </div>
-                                </div> <!-- slider product image -->
-                            </div>
-                            <div class="col-lg-8 col-md-7"style="background: linear-gradient(30deg,hsla(0,0%,100%,.1),rgba(71,144,240,.507))">
-                                <div class="slider-product-content">
-                                    <h1 class="slider-title mb-10" data-animation="fadeInUp" data-delay="0.3s"><span>short</span> <span> sleeves</span> <span>shirt</span></h1>
-                                    <p class="mb-25" data-animation="fadeInUp" data-delay="0.9s">This shirt is made in Indonesia<br> and 50% discount let's buy.</p>
-                                    <a class="main-btn" href="{{ url('/home') }}" data-animation="fadeInUp" data-delay="1.5s"style="background: linear-gradient(30deg,hsla(0,0%,100%,.1),rgba(71,000,240,.507))">Explore More <i class="lni-chevron-right"></i></a>
-                                </div> <!-- slider product content -->
-                            </div>
-                        </div> <!-- row -->
-                    </div> <!-- container -->
-                </div> <!-- single slider -->
-
-                <div class="single-slider">
-                        <div class="">
-                            <div class="row no-gutters align-items-center">
-                                <div class="col-lg-4 col-md-5">
-                                    <div class="slider-product-image d-none d-md-block">
-                                        <img src="assets/images/slider/kemeja30.jpg" alt="Slider">
-                                        <div class="slider-discount-tag">
-                                            <p>-20% <br> OFF</p>
-                                        </div>
-                                    </div> <!-- slider product image -->
-                                </div>
-                                <div class="col-lg-8 col-md-7"style="background: linear-gradient(30deg,hsla(0,0%,100%,.1),rgba(71,144,240,.507))">
-                                    <div class="slider-product-content">
-                                        <h1 class="slider-title mb-10" data-animation="fadeInUp" data-delay="0.3s"><span>long</span> <span> sleeves</span> <span>shirt</span></h1>
-                                        <p class="mb-25" data-animation="fadeInUp" data-delay="0.9s">This shirt is made in Indonesia<br> and 20% discount let's buy.</p>
-                                        <a class="main-btn" href="{{ url('/home') }}" data-animation="fadeInUp" data-delay="1.5s"style="background: linear-gradient(30deg,hsla(0,0%,100%,.1),rgba(71,000,240,.507))">Explore More <i class="lni-chevron-right"></i></a>
-                                    </div> <!-- slider product content -->
-                                </div>
-                            </div> <!-- row -->
-                        </div> <!-- container -->
-                </div> <!-- single slider -->
-
-                <div class="single-slider" >
-                        <div class="" >
-                            <div class="row no-gutters align-items-center">
-                                <div class="col-lg-4 col-md-5">
-                                    <div class="slider-product-image d-none d-md-block">
-                                        <img src="assets/images/slider/kemeja2.jpg" alt="Slider">
-                                        <div class="slider-discount-tag">
-                                            <p>FREE<br>SHIPPING</p>
-                                        </div>
-                                    </div> <!-- slider product image -->
-                                </div>
-                                <div class="col-lg-8 col-md-7" style="background: linear-gradient(30deg,hsla(0,0%,100%,.1),rgba(71,144,240,.507))">
-                                    <div class="slider-product-content" >
-                                        <h1 class="slider-title mb-10" data-animation="fadeInUp" data-delay="0.3s"><span>short</span> <span> sleeves</span> <span>shirt</span></h1>
-                                        <p class="mb-25" data-animation="fadeInUp" data-delay="0.9s">This shirt is made in Indonesia<br> and free shipping let's buy.</p>
-                                        <a class="main-btn text-light" href="{{ url('/home') }}" data-animation="fadeInUp" data-delay="1.5s"style="background: linear-gradient(30deg,hsla(0,0%,100%,.1),rgba(71,000,240,.507))">Explore More <i class="lni-chevron-right"></i></a>
-                                    </div> <!-- slider product content -->
-                                </div>
-                            </div> <!-- row -->
-                        </div> <!-- container -->
-                </div> <!-- single slider -->
-            </div> <!-- slider active -->
-           <hr>
-        </div> <!-- container fluid -->
-    </section>
-    
-    <!--====== SLIDER PART ENDS ======-->
+     
    
- 
+  <div class="FrontendVue">
+
+  </div>
    
     <!--====== PRODUCT PART START ======-->
     
