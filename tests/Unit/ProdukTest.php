b5e5037e68c42f2e802dc39d9bc0c4f3229b6375
i ketut karmita <100656272+iktkarmita@users.noreply.github.com>
2022-06-11 13:39:04 +0800

    update test unit

--- tests/Unit/ProdukTest.php
+++ tests/Unit/ProdukTest.php
@@ -1,31 +1,79 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Feature;
 
-use App\Http\Controllers;
-use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
 
-class ProdukTest extends TestCase
+class ShowPage extends TestCase
 {
-
-    use WithFaker;
-
     /**
-     * A basic unit test example.
+     * A basic feature test example.
      *
      * @return void
      */
-    public function test_produk_index()
+    public function test_homePage()
     {
-        $value = [
-            'nama' => $this->faker->name(),
-            'keterangan' => $this->faker->text(),
-            'harga' => $this->faker->randomElement([1000, 2000, 4000, 5000, 10000]),
-            'persediaan' => $this->faker->randomElement([7, 10, 15, 20, 30]),
-            'image' => $this->faker->imageUrl(),
-        ];
+        $response = $this->get('/');
 
-        $this->get(route('produks.index'), $value)->assertStatus(200); //Test Produk Controller Function index berjalan 200ok
+        $response->assertStatus(200);
+    }
+    public function test_register()
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+    public function test_login()
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+    public function test_home_prduk()
+    {
+        $response = $this->get('/home'); //gabisa di akses karna karus login
+
+        $response->assertStatus(200);
+    }
+    public function test_product()
+    {
+        $response = $this->get('/post'); //gabisa di akses karna karus login
+
+        $response->assertStatus(200);
+    }
+
+    public function test_edit()
+    {
+        $response = $this->get('/edit');
+
+        $response->assertStatus(200);
+    }
+    public function test_Create() //CREATE
+    {
+        $response = $this->post('/post/store');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_Read() //Read
+    {
+        $response = $this->get('/edit/2');
+        $response->assertStatus(200);
+    }
+
+    public function test_update() //update
+    {
+        $response = $this->put('/edit/post/2');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_Delete() //delete
+    {
+        $response = $this->get('/edit/post/delete/2');
+
+        $response->assertStatus(200);
     }
 }
